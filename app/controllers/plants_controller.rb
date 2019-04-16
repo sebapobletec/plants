79c5f401d4d98625f2b206fa2a5a6fa1a6d633a0
Sebastian Poblete <sebapobletec@gmail.com>
2019-04-15 21:49:19 -0400

    CanCanCan

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,7 @@
 class PlantsController < ApplicationController
-  before_action :set_plant, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  #before_action :set_plant, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
+
   # GET /plants
   # GET /plants.json
   def index
